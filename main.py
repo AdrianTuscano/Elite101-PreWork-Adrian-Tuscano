73bf228f7958450ac386ef4b8f6e9178016a49b8
Adrian Tuscano <141881245+AdrianTuscano@users.noreply.github.com>
2024-09-19 04:27:01 +0000

    Added text after reciveing user inputs

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ print("Welcome to Adrian's Elite-101 Chatbot!")
 # Collect the user’s name and age
 name = input("Please input your name:")
 age = int(input("Please input your age:"))
+if(age<15):
+    print("Wow " + name + " you are younger than me!")
+elif(15>=age<18):
+    print("Wow " + name +  " you must be in highschool!")
+elif(18>=age<55):
+    print("Wow " + name + " you must be an adult!")
+elif(55>=age<120):
+    print("Wow " + name + " you are older than my parents!")
+elif(age>120):
+    print("Wow " + name + " you might break a record!")
 # Ask the user how it can help them
 
 # Allow the user to choose from a menu/list of options on how they can continue the conversation

97bac6ebb078e08dc8c337e18520fb2373c4bf40
Adrian Tuscano <141881245+AdrianTuscano@users.noreply.github.com>
2024-09-19 04:13:14 +0000

    Added code to collect the users name and age, made sure the age was stored as an integer

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 # Welcome the user
 print("Welcome to Adrian's Elite-101 Chatbot!")
 # Collect the user’s name and age
-
+name = input("Please input your name:")
+age = int(input("Please input your age:"))
 # Ask the user how it can help them
 
 # Allow the user to choose from a menu/list of options on how they can continue the conversation

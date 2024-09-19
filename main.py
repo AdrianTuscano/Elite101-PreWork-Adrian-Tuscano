9d9358da6a2b1176134ca61572d06a1d32edc9f1
Adrian Tuscano <141881245+AdrianTuscano@users.noreply.github.com>
2024-09-19 04:05:12 +0000

    Initialized Chatbot Project Wrote simple outline of the projects structure.

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello CodeSandbox!!!!!")
+# Welcome the user
+
+# Collect the user’s name and age
+
+# Ask the user how it can help them
+
+# Allow the user to choose from a menu/list of options on how they can continue the conversation
+
+# Add Exit Option in Menu
